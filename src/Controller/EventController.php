e56b48816de35f78a702a7995e0680159279c8a0
Matthieu Mota <matthieumota@gmail.com>
2022-09-01 14:32:43 +0200

    Templates

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,10 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
+    protected $events = [
+        ['id' => 1, 'name' => 'Concert 1', 'description' => 'Lorem', 'price' => 10, 'createdAt' => '2022-08-28 11:38:45', 'startAt' => '2022-08-28 11:38:45', 'endAt' => '2022-08-29 11:38:45'],
+        ['id' => 2, 'name' => 'Concert 2', 'description' => 'Lorem', 'price' => 20, 'createdAt' => '2022-08-30 11:38:45', 'startAt' => '2022-08-30 11:38:45', 'endAt' => '2022-09-02 11:38:45'],
+        ['id' => 3, 'name' => 'Concert 3', 'description' => 'Lorem', 'price' => 30, 'createdAt' => '2022-09-02 11:38:45', 'startAt' => '2022-09-02 11:38:45', 'endAt' => '2022-09-03 11:38:45'],
+    ];
+
     #[Route('/evenements', name: 'app_event')]
     public function index(): Response
     {
-        return $this->render('event/index.html.twig');
+        return $this->render('event/index.html.twig', [
+            'events' => $this->events,
+            'incoming' => count(array_filter($this->events, function ($event) {
+                return new \DateTime($event['startAt']) > new \DateTime();
+            })),
+        ]);
     }
 
     #[Route('/evenement/nouveau', name: 'app_event_create')]
@@ -23,6 +34,10 @@ class EventController extends AbstractController
     #[Route('/evenement/{id}', name: 'app_event_show')]
     public function show($id): Response
     {
+        if (! in_array($id, array_column($this->events, 'id'))) {
+            throw $this->createNotFoundException();
+        }
+
         return $this->render('event/show.html.twig', [
             'id' => $id,
         ]);
